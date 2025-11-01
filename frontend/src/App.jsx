6814dde1d8cff5509248a7d6c479bd6cf76bd2e9
Abhi-K-Abhi <abhipatel120801@gmail.com>
2025-11-01 13:42:37 -0400

    Initial |React app setup using Vite.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>🌦️ Weather Dashboard</h1>
+      <p>Welcome! Let’s build this together step by step.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ test.js
@@ -0,0 +1,5 @@
+// function to add two numbers
+
+function add(a, b) {
+    return a + b;
+}
